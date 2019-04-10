c57d2b6e1c88592c211838dbd176536d1f13645f
Robert Gibboni <galileo@gmail.com>
2019-04-10 16:41:26 -0700

    Add url for water system locations

--- src/config.py
+++ src/config.py
@@ -5,3 +5,4 @@ DATA_DIRECTORY = (Path(__file__).parents[1] / 'data').resolve()
 
 HR2W_EXCEEDANCE_URL = "https://www.waterboards.ca.gov/water_issues/programs/hr2w/docs/data/hr2w_web_data_active_2-2019.xlsx"
 HR2W_RETURN_TO_COMPLIANCE_URL = "https://www.waterboards.ca.gov/water_issues/programs/hr2w/docs/data/hr2w_web_data_rtc_2-2019.xlsx"
+WATER_SYSTEM_LOCATIONS_URL = "https://www.waterboards.ca.gov/water_issues/programs/hr2w/docs/data/ec_summary_feb2019.zip"
