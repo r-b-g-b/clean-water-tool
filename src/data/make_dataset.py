58cef2923b86cdba095efa0ea8dcdcafbd39c976
Robert Gibboni <robert@drivendata.org>
2019-04-12 18:46:16 -0700

    Make command for getting water system lat/lon

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import shapefile
+from json import dumps
+import urllib
+from pathlib import Path
+import io
+import zipfile
 import click
 import logging
 import pandas as pd
@@ -6,8 +12,75 @@ import requests
 import os
 
 from src.config import (
-    HR2W_EXCEEDANCE_URL, HR2W_RETURN_TO_COMPLIANCE_URL, DATA_DIRECTORY
+    DATA_DIRECTORY,
+    HR2W_EXCEEDANCE_URL,
+    HR2W_RETURN_TO_COMPLIANCE_URL,
+    WATER_SYSTEM_LOCATIONS_URL,
 )
+
+
+def make_hr2w_data():
+    data = requests.get(HR2W_EXCEEDANCE_URL)
+
+    if not os.path.exists(DATA_DIRECTORY / "raw"):
+        os.makedirs(DATA_DIRECTORY / "raw")
+    if not os.path.exists(DATA_DIRECTORY / "interim"):
+        os.makedirs(DATA_DIRECTORY / "interim")
+
+    with open(DATA_DIRECTORY / "raw" / "hr2w_exceedance.xlsx", "wb") as f:
+        f.write(data.content)
+
+    data = requests.get(HR2W_RETURN_TO_COMPLIANCE_URL)
+
+    with open(DATA_DIRECTORY / "raw" / "hr2w_return_to_compliance.xlsx", "wb") as f:
+        f.write(data.content)
+
+    df = pd.read_excel(DATA_DIRECTORY / "raw" / "hr2w_exceedance.xlsx")
+    df.to_csv(DATA_DIRECTORY / "interim" / "hr2w_exceedance.csv", index=False)
+
+    df = pd.read_excel(DATA_DIRECTORY / "raw" / "hr2w_return_to_compliance.xlsx")
+    df.to_csv(DATA_DIRECTORY / "interim" / "hr2w_return_to_compliance.csv", index=False)
+
+
+def make_water_system_latlon():
+    # from https://stackoverflow.com/questions/43119040/shapefile-into-geojson-conversion-python-3
+    data = requests.get(WATER_SYSTEM_LOCATIONS_URL)
+    zpfile = zipfile.ZipFile(io.BytesIO(data.content))
+
+    output_directory = Path(
+        DATA_DIRECTORY /
+        "raw" /
+        Path(urllib.parse.urlparse(WATER_SYSTEM_LOCATIONS_URL).path).stem
+    )
+    output_directory.mkdir(parents=True, exist_ok=True)
+
+    zpfile.extractall(output_directory)
+
+    shp_filename = [
+        p.filename for p in zpfile.infolist() if p.filename.endswith(".shp")
+    ][0]
+
+    reader = shapefile.Reader(str(output_directory / shp_filename))
+    fields = reader.fields[1:]
+
+    field_names = [field[0] for field in fields]
+    features = []
+    for record in reader.shapeRecords():
+        atr = dict(zip(field_names, record.record))
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": record.shape.__geo_interface__,
+                "properties": atr,
+            }
+        )
+
+    # write the GeoJSON file
+    with open(DATA_DIRECTORY / "interim" / "water_system_latlon.geojson", "w") as f:
+        f.write(
+            dumps({"type": "FeatureCollection", "features": features}, indent=2)
+            + "\n"
+        )
 
 
 @click.command()
@@ -16,33 +89,18 @@ def main():
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-    data = requests.get(HR2W_EXCEEDANCE_URL)
 
-    if not os.path.exists(DATA_DIRECTORY / 'raw'):
-        os.makedirs(DATA_DIRECTORY / 'raw')
-    if not os.path.exists(DATA_DIRECTORY / 'interim'):
-        os.makedirs(DATA_DIRECTORY / 'interim')
+    logger.info("making hr2w dataset")
+    make_hr2w_data()
 
-    with open(DATA_DIRECTORY / 'raw' / 'hr2w_exceedance.xlsx', 'wb') as f:
-        f.write(data.content)
-
-    data = requests.get(HR2W_RETURN_TO_COMPLIANCE_URL)
-
-    with open(DATA_DIRECTORY / 'raw' / 'hr2w_return_to_compliance.xlsx', 'wb') as f:
-        f.write(data.content)
-
-    df = pd.read_excel(DATA_DIRECTORY / 'raw' / 'hr2w_exceedance.xlsx')
-    df.to_csv(DATA_DIRECTORY / 'interim' / 'hr2w_exceedance.csv', index=False)
-
-    df = pd.read_excel(DATA_DIRECTORY / 'raw' / 'hr2w_return_to_compliance.xlsx')
-    df.to_csv(DATA_DIRECTORY / 'interim' / 'hr2w_return_to_compliance.csv', index=False)
+    logger.info("making water system latitude/longitude dataset")
+    make_water_system_latlon()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
     main()
